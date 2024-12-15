5be564ca4fbec781cfc61292a9b008f0d40e3760
Mohsin Ali <khapluvi38@gmail.com>
2024-12-15 18:04:01 +0500

    layouts

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,45 +1,72 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const Layout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: 'mon-sb',
+        },
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name='search' size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name='whishlists'
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarLabel: 'Wishlists',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name='heart-outline' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='trips'
+        options={{
+          tabBarLabel: 'Trips',
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome5 name='airbnb' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='inbox'
+        options={{
+          tabBarLabel: 'Inbox',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name='message-outline'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarLabel: 'Profile',
+
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name='person-circle-outline' size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default Layout;
